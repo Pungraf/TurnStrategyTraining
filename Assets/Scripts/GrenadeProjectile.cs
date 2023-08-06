4607cfe07f3d54af990db705a504d704aa8a58a5
Jan Bartłomiejczyk <44816218+Pungraf@users.noreply.github.com>
2023-08-06 22:35:57 +0200

    Grenade Action

---
+++ Assets/Scripts/GrenadeProjectile.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrenadeProjectile : MonoBehaviour
+{
+    public static event EventHandler OnAnyGrenadeExploded;
+
+    [SerializeField] private Transform grenadeExplodeVfxPrefab;
+    [SerializeField] private TrailRenderer trailRenderer;
+    [SerializeField] private AnimationCurve arcYAnimationCurve;
+
+    private Vector3 targetPosition;
+    private Action OnGrenadeBehaviourComplete;
+    private float totalDistance;
+    private Vector3 positionXZ;
+
+    private void Update()
+    {
+        Vector3 moveDir = (targetPosition - positionXZ).normalized;
+
+        float moveSpeed = 15f;
+        positionXZ += moveDir * moveSpeed * Time.deltaTime;
+
+        float distance = Vector3.Distance(positionXZ, targetPosition);
+        float distanceNomalized = 1 - distance / totalDistance;
+
+        float maxHeight = totalDistance / 4f;
+        float positionY = arcYAnimationCurve.Evaluate(distanceNomalized) * maxHeight;
+        transform.position = new Vector3(positionXZ.x, positionY, positionXZ.z);
+
+        float reachedTargetDistance = .2f;
+        if(Vector3.Distance(positionXZ, targetPosition) < reachedTargetDistance)
+        {
+            float radius = 4f;
+            Collider[] colliderArray = Physics.OverlapSphere(targetPosition, radius);
+            foreach(Collider collider in colliderArray)
+            {
+                if(collider.TryGetComponent<Unit>(out Unit targetUnit))
+                {
+                    targetUnit.Damage(30);
+                }
+            }
+
+            OnAnyGrenadeExploded?.Invoke(this, EventArgs.Empty);
+
+            trailRenderer.transform.parent = null;
+
+            Instantiate(grenadeExplodeVfxPrefab, targetPosition + Vector3.up * 1f, Quaternion.identity);
+            Destroy(gameObject);
+
+            OnGrenadeBehaviourComplete();
+        }
+    }
+
+    public void Setup(GridPosition targetGridPosition, Action OnGrenadeBehaviourComplete)
+    {
+        this.OnGrenadeBehaviourComplete = OnGrenadeBehaviourComplete;
+        targetPosition = LevelGrid.Instance.GetWorldPosition(targetGridPosition);
+
+        positionXZ = transform.position;
+        positionXZ.y = 0;
+
+        totalDistance = Vector3.Distance(positionXZ, targetPosition);
+    }
+}
